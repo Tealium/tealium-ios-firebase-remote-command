cf1563af51d5aa67417a603d9d742768ea4218cb
christinasund <christina.sund@tealium.com>
2021-05-14 11:35:59 -0700

    initial update - wip

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+    name: "TealiumFirebase",
+    platforms: [
+        .iOS(.v10)
+    ],
+    products: [
+        .library(name: "TealiumFirebase", targets: ["TealiumFirebase"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/tealium/tealium-swift", from: "2.3.0"),
+        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "8.0.0")
+    ],
+    targets: [
+        .target(
+            name: "TealiumFirebase",
+            dependencies: ["Adjust", "TealiumCore", "TealiumRemoteCommands", "TealiumTagManagement", "TealiumCollect"],
+            path: "./Sources")
+        .testTarget(
+            name: "TealiumFirebaseTests",
+            dependencies: ["TealiumFirebase"],
+            path: "./Tests")
+    ]
+)
